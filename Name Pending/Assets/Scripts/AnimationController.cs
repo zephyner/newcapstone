469451c59a96e8df2a8c00729cb0b2495657fe4d
Kymani1001 <Merriott416@icloud.com>
2021-03-19 23:59:37 -0400

    Updated player movement

---
+++ Name Pending/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    Animator anima;
+    int walkingForwardHash;
+    int walkingBackHash;
+    int walkingLeftHash;
+    int walkingRightHash;
+
+    int runningForwardHash;
+    int runningBackHash;
+    int runningLeftHash;
+    int runningRightHash;
+
+    int jumpHash;
+    int isGroundedHash;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anima = GetComponent<Animator>();
+
+        //Corrisponds to the string in the animator
+        walkingForwardHash = Animator.StringToHash("isWalkingForward");
+        walkingBackHash = Animator.StringToHash("isWalkingBack");
+        walkingLeftHash = Animator.StringToHash("isWalkingLeft");
+        walkingRightHash = Animator.StringToHash("isWalkingRight");
+
+        runningForwardHash = Animator.StringToHash("isRunningForward");
+        runningBackHash = Animator.StringToHash("isRunningBack");
+        runningLeftHash = Animator.StringToHash("isRunningLeft");
+        runningRightHash = Animator.StringToHash("isRunningRight");
+
+        jumpHash = Animator.StringToHash("jump");
+        isGroundedHash = Animator.StringToHash("isGrounded");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Corrisponds to the walkingHash of that direction
+        bool isWalking_F = anima.GetBool(walkingForwardHash);
+        bool isWalking_B = anima.GetBool(walkingBackHash);
+        bool isWalking_L = anima.GetBool(walkingLeftHash);
+        bool isWalking_R = anima.GetBool(walkingRightHash);
+        
+        //Checks if we are already running
+        bool isRunning_F = anima.GetBool(runningForwardHash);
+        bool isRunning_B = anima.GetBool(runningBackHash);
+        bool isRunning_L = anima.GetBool(runningLeftHash);
+        bool isRunning_R = anima.GetBool(runningRightHash);
+
+        //
+        bool isJumping = anima.GetBool(jumpHash);
+        bool grounded = anima.GetBool(isGroundedHash);
+
+        //The direction that uses the animation
+        bool forwardInput = Input.GetKey("w");
+        bool backInput = Input.GetKey("s");
+        bool leftInput = Input.GetKey("a");
+        bool rightInput = Input.GetKey("d");
+        bool runInput = Input.GetKey("left shift");
+
+        bool jumpInput = Input.GetKey("space");
+
+        
+
+        //Used for the input to walk when pressed
+        if (!isWalking_F && forwardInput) 
+        {
+            anima.SetBool("isWalkingForward", true);
+        }
+        else if (!isWalking_R && backInput)
+        {
+            anima.SetBool("isWalkingBack", true);
+        }
+        else if (!isWalking_L && leftInput)
+        {
+            anima.SetBool("isWalkingLeft", true);
+        }
+        else if (!isWalking_R && rightInput)
+        {
+            anima.SetBool("isWalkingRight", true);
+        }
+
+        //Used for the input to walk when not being pressed
+        if (isWalking_F && !forwardInput)
+        {
+            anima.SetBool("isWalkingForward", false);
+        }
+        else if (isWalking_B && !backInput)
+        {
+            anima.SetBool("isWalkingBack", false);
+        }
+        else if (isWalking_L && !leftInput)
+        {
+            anima.SetBool("isWalkingLeft", false);
+        }
+        else if (isWalking_R && !rightInput)
+        {
+            anima.SetBool("isWalkingRight", false);
+        }
+
+        //Used for when walking, and not running when holding the run button
+        if (!isRunning_F && (forwardInput && runInput))
+        {
+            anima.SetBool("isRunningForward", true);
+        }
+        else if (!isRunning_B && (backInput && runInput))
+        {
+            anima.SetBool("isRunningBack", true);
+        }
+        else if (!isRunning_L && (leftInput && runInput))
+        {
+            anima.SetBool("isRunningLeft", true);
+        }
+        else if (!isRunning_R && (rightInput && runInput))
+        {
+            anima.SetBool("isRunningRight", true);
+        }
+
+        //Used for when running and stops running or walking
+        if (isRunning_F && (!forwardInput || !runInput))
+        {
+            anima.SetBool("isRunningForward", false);
+        }
+        else if (isRunning_B && (!backInput || !runInput))
+        {
+            anima.SetBool("isRunningBack", false);
+        }
+        else if (isRunning_L && (!leftInput || !runInput))
+        {
+            anima.SetBool("isRunningLeft", false);
+        }
+        else if (isRunning_R && (!rightInput || !runInput))
+        {
+            anima.SetBool("isRunningRight", false);
+        }
+
+        //Used for jumping
+        if (!isJumping && jumpInput)
+        {
+            anima.SetBool("jump", true);
+
+
+        }
+        else if (isJumping && !jumpInput)
+        {
+            anima.SetBool("jump", false);
+        }
+
+        //Checks if the player is grounded
+        if (!grounded && anima.GetBool("isGrounded"))    
+        {
+            anima.SetBool("isGrounded", true);
+        }
+        else if (grounded && !anima.GetBool("isGrounded"))
+        {
+            anima.SetBool("isGrounded", false);
+        }
+    }
+}
---
+++ Name Pending/Assets/Scripts/Player.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+    public Transform groundCheck;
+
+    public float speed = 6f;
+
+    //To make the turning look more natural on the movement
+    public float smoothTurnTime = 0.1f;
+    private float smoothTurnSpeed;
+
+    public float gravity = -9.81f;
+    public float groundDist = 0.4f;
+    public LayerMask groundMask;
+    Vector3 velocity;
+
+    public float jump = 3f;
+    bool isGrounded;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        //Helps ensure the velocity isn't increasing as we are grounded
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDist, groundMask);
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jump * -2f * gravity);
+        }
+
+        float hori = Input.GetAxisRaw("Horizontal");        
+        float vert = Input.GetAxisRaw("Vertical");
+      
+        //Ensures there is no y-axis movement when moving on the ground
+        Vector3 dir = new Vector3(hori, 0f, vert).normalized;
+
+        //Checks if there is a direction the player is moving in
+        if (dir.magnitude >= 0.1f)
+        {
+            //used to rotate the z-axis to face the direction the player is facing
+            float lookingDir = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, lookingDir, ref smoothTurnSpeed, smoothTurnTime);
+
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            //Helps to have the camera and player move in the same direction
+            Vector3 moveDir = Quaternion.Euler(0f, lookingDir, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+
+
+        //The gravity of how fast the player falls
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
